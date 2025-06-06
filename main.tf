122341026b0105802bd9944a358698a12db07eff
Vikas-git <Vikasshetty543@gmail.com>
2025-06-06 16:33:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-provider "aws" {
+provider "aws"{
     region = "eu-north-1"
-    access_key = " "
-    secret_key = " "
 }
 resource "aws_instance" "ec2" {
-    ami = "ami-0c1ac8a41498c1a9c"
-    instance_type = "t3.medium"
+    ami = "ami-006b4a3ad5f56fbd6"
+    instance_type = "t3.micro"
     key_name = "private_key_1"
     subnet_id = "subnet-0ab3aadf95217a62c"
+    vpc_security_group_ids = ["sg-0937d86d5ba835424"]
     associate_public_ip_address = true 
     root_block_device{
-        volume_size = 16
+        volume_size = 8
         volume_type ="gp2"
     }
-    user_data = file("script.sh")
     tags = {
-        Name = "ec2_by_terraform"
+        Name = "JENKINS_TERRAFORM_INT"
     }
 }
