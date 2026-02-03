6d34deaa7c4eb7b51a44ae1843f2f6ab77741969
Vikas-git <Vikasshetty543@gmail.com>
2026-02-03 09:59:01 +0530

    Add files via upload

---
+++ EC2/modules/ec2_instance/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
